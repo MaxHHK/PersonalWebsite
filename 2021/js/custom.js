59671d57dcf4771eee1e90ea7f84ae04496d6058
max <1913147093@qq.com>
2021-12-30 17:13:09 +0800

    Test more pop up

--- 2021/js/custom.js
+++ 2021/js/custom.js
@@ -104,7 +104,8 @@ $(document).ready(function () {
     });
 
     //animatedModal
-    $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
+    $("#demo01").animatedModal();
+    $("#demo02").animatedModal02();
 
     // Contact Form 	
 
@@ -163,3 +164,39 @@ $(document).ready(function () {
 
     });
 });
+
+
+// show pop up
+function ShowDiv(show_div, bg_div){
+    document.getElementById(show_div).style.display='block';
+    document.getElementById(bg_div).style.display='block';
+    var bgdiv = document.getElementById(bg_div);
+    bgdiv.style.width = document.body.scrollWidth;
+    // bgdiv.style.height = $(document).height();
+    $("#"+bg_div).height($(document).height());
+};
+
+// close pop up
+function CloseDiv(show_div, bg_div)
+{
+    document.getElementById(show_div).style.display='none';
+    document.getElementById(bg_div).style.display='none';
+};
+
+
+// show more
+function ShowMore(show_div){
+    document.getElementById(show_div).style.display='block';
+    var sdiv = document.getElementById(show_div);
+    sdiv.style.width = document.body.scrollWidth;
+    // bgdiv.style.height = $(document).height();
+    $("#"+sdiv).height($(document).height());
+    sdiv.style.display = 'none';
+};
+
+// show less
+function ShowLess(show_div, bg_div)
+{
+    document.getElementById(show_div).style.display='none';
+    document.getElementById(bg_div).style.display='none';
+};
